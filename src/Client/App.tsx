3514d09ea04583e94a62dad3c821bed460096004
cmagorr1 <71301902+cmagorr1@users.noreply.github.com>
2024-06-01 16:51:33 -0400

    menubar and input/output boxes

--- src/Client/App.tsx
+++ src/Client/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Landing from './Pages/Landing';
+import Main from './Pages/Main';
+import { useState } from 'react';
 
 const App: React.FunctionComponent = () => {
+    const [inputText, setInputText] = useState('');
+    const [outputText, setOutputText] = useState('');
+
+
+
     return (
         <div>
-            <h1>Hello</h1>
+            <Main/>
         </div>
     )
 }
