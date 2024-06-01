ec40cf259cc40f4fde9eb8ee6070832ec5b55c64
Russell Apolonio <russell1740@gmail.com>
2024-06-01 09:58:56 -0700

    commented out frontend

--- src/Client/Components/InputBox.tsx
+++ src/Client/Components/InputBox.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
+// import React from 'react';
 
-interface InputBox {
-    label: string;
-    placeholder?: string;
-    value: string;
-    onChange: (value: string) => void;
-  }
+// interface InputBox {
+//     label: string;
+//     placeholder?: string;
+//     value: string;
+//     onChange: (value: string) => void;
+//   }
 
-const InputBox: React.FunctionComponent = ({ label, placeholder, value, onChange }) => {
+// const InputBox: React.FunctionComponent = ({ label, placeholder, value, onChange }) => {
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        onChange(event.target.value);
-      };
+//     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         onChange(event.target.value);
+//       };
 
-    return (
-        <div>
-            <label htmlFor={label}>{label}</label>
-            <input
-        type="text"
-        id={label}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleChange}
-            />
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <label htmlFor={label}>{label}</label>
+//             <input
+//         type="text"
+//         id={label}
+//         placeholder={placeholder}
+//         value={value}
+//         onChange={handleChange}
+//             />
+//         </div>
+//     )
+// }
 
-export default InputBox;
+// export default InputBox;
--- src/Client/Pages/Landing.tsx
+++ src/Client/Pages/Landing.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+// import React from 'react';
 
-const Landing: React.FunctionComponent = () => {
+// const Landing: React.FunctionComponent = () => {
 
-  return ()
-}
+//   return ()
+// }
 
-export default Landing;
+// export default Landing;
