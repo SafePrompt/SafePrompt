8b9fd37406fc2a05856a113a446a91d2e0beaf20
rmarkowi1990 <rick@fastcasual.co>
2024-06-02 22:07:02 -0700

    working minus parenthesees

--- src/Server/Controllers/Validation/phone.ts
+++ src/Server/Controllers/Validation/phone.ts
@@ -17,79 +17,170 @@ asdf1 1(585)752-5306
 
 const phone: middleware = (req,res,next)=>{
 
-    const prompt: string = res.locals.prompt;
+    try{
+    
+        let prompt:string = res.locals.prompt;
 
-    let failed:string[] = [];
+        console.log('prompt: ', prompt)
 
-    let digits:string = '';
+        const string = 'hello'
 
-    for (let i: number = 0; i < prompt.length; i++) {
+        console.log('slice check', string.slice(5))
 
-        digits = '';
+        const phone1:RegExp = /\d{10}/;
+        const phone2:RegExp = /\d{3}-\d{3}-\d{4}/;
+        const phone3:RegExp = /(\d{3})\d{3}-\d{4}/;
+        const phone4:RegExp = /(\d{3})-\d{3}-\d{4}/;
+        const phone5:RegExp = /\d{3}.\d{3}.\d{4}/;
+        const phone6:RegExp = /\d{3} \d{3} \d{4}/;
+        const phone7:RegExp = /(\d{3}) \d{3} \d{4}/;
+        const phone8:RegExp = /\+\d{11}/;
+        const phone9:RegExp = /\+\d{1} \d{3}.\d{3}.\d{4}/;
+        const phone10:RegExp = /\+\d{1} \d{3}-\d{3}-\d{4}/;
+        const phone11:RegExp = /\+\d{1} (\d{3}) \d{3} \d{4}/;
+        const phone12:RegExp = /\+\d{12}/;
 
-        if (!isNaN(Number(prompt[i])) && prompt[i]!==' ' || prompt[i] === '(' || prompt[i] === '+'){
-            for(let characters:number = 10; characters<=17; characters++){
+        console.log('prompt check: ', phone2.test(prompt))
+
+        if (phone1.test(prompt) ||
+            phone2.test(prompt) ||
+            phone3.test(prompt) ||
+            phone4.test(prompt) ||
+            phone5.test(prompt) ||
+            phone6.test(prompt) ||
+            phone7.test(prompt) ||
+            phone8.test(prompt) ||
+            phone9.test(prompt) ||
+            phone10.test(prompt) ||
+            phone11.test(prompt) ||
+            phone12.test(prompt)
+        ){
+            const failed: string[] = [];
+
+            console.log("inside")
+
+            let i:number = 0;
+            const startExp = /[1234567890+(]/
+
+            while (phone1.test(prompt) ||
+            phone2.test(prompt) ||
+            phone3.test(prompt) ||
+            phone4.test(prompt) ||
+            phone5.test(prompt) ||
+            phone6.test(prompt) ||
+            phone7.test(prompt) ||
+            phone8.test(prompt) ||
+            phone9.test(prompt) ||
+            phone10.test(prompt) ||
+            phone11.test(prompt) ||
+            phone12.test(prompt)
+            ){
+
+                console.log('prompt: ', prompt, ' i: ', i)
+
+                if(startExp.test(prompt[i])){
+                    
+                    let holder:string = ''
+
+                    if (phone1.test(prompt.slice(i,i+10))){
+
+                        failed.push(prompt.slice(i,i+10));
+                        prompt = prompt.slice(i+10);
+                        i=0;
+
+                    }else if (phone2.test(prompt.slice(i,i+12))){
+
+                        failed.push(prompt.slice(i,i+12));
+                        prompt = prompt.slice(i+12);
+                        console.log('this is a prompt: ', prompt)
+                        i=0;
+
+                    }else if (phone3.test(prompt.slice(i,i+13))){
+
+                        failed.push(prompt.slice(i, i+13));
+                        prompt = prompt.slice(i+13);
+                        i=0;
+
+                    }else if (phone4.test(prompt.slice(i,i+14))){
+
+                        failed.push(prompt.slice(i, i+14));
+                        prompt = prompt.slice(i+14);
+                        i=0;
+
+                    }else if (phone5.test(prompt.slice(i,i+12))){
+
+                        failed.push(prompt.slice(i, i+12));
+                        prompt = prompt.slice(i+12);
+                        i=0;
+                    
+                    }else if (phone6.test(prompt.slice(i,i+12))){
+
+                        failed.push(prompt.slice(i, i+12));
+                        prompt = prompt.slice(i+12);
+                        i=0;
+
+                    }else if (phone7.test(prompt.slice(i,i+14))){
+
+                        failed.push(prompt.slice(i, i+14));
+                        prompt = prompt.slice(i+14);
+                        i=0;
+
+                    }else if (phone8.test(prompt.slice(i,i+12))){
+
+                        failed.push(prompt.slice(i, i+12));
+                        prompt = prompt.slice(i+12);
+                        i=0;
+                    
+                    }else if (phone9.test(prompt.slice(i,i+15))){
+
+                        failed.push(prompt.slice(i, i+15));
+                        prompt = prompt.slice(i+15);
+                        i=0;
+
+                    }else if (phone10.test(prompt.slice(i,i+15))){
+
+                        failed.push(prompt.slice(i, i+15));
+                        prompt = prompt.slice(i+15);
+                        i=0;
+                    
+                    }else if (phone11.test(prompt.slice(i,i+17))){
+
+                        failed.push(prompt.slice(i, i+17));
+                        prompt = prompt.slice(i+17);
+                        i=0;
+                    
+                    }else if (phone12.test(prompt.slice(i,i+13))){
  
-                digits = prompt.slice(i,i+characters);
-             
-                if(isValidPhoneNumber(digits, 'US')){
-         
-                    failed.push(digits);
-                    i = i+characters;
+                        failed.push(prompt.slice(i, i+13));
+                        prompt = prompt.slice(i+13);
+                        i=0;
+                    }
+
+                    else {i++
+                    }
+
+
+                }else{
+                    i++
                 }
-              
-            }
-           
-        } 
-      
+
+
+
+                
+             }
+
+             console.log('failed: ', failed)
+
     }
 
-    let badIndex: number[];
 
-    if (failed.length > 0){
+    
 
-        //checking for misplaced periods, dashes
+    return next();
 
-        failed = failed.filter((str:string) : boolean => {
-            if (str.includes('-')||str.includes('.')){            
-                if (str[0] === '('){
-                    badIndex = [0,1,2,3,4,6,7,10,11,12];
-                    if (!(str[5] === " " || str[5] === "-")) {
-                        badIndex.push(5);
-                    } 
-                    for (let el of badIndex){
-                        if (str[el] === '.' || str[el] ==='-'){
-                            return false;
-                        }
-                    }
-                    return true;            
-                }
-                else if (str.length === 11){
-                    return false;
-                }
-                else{
-                    badIndex = [0,1,2,4,5,6,8,10,11]
-                    for (let el of badIndex){
-                        if (str[el] === '.' || str[el] ==='-'){
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-            } else {
-                return true;
-            }
-        }).filter((str:string): boolean =>{
-            if (str.includes('(') && !str.includes(')')) return false;
-            else if (str.includes('-') && str.includes('.')) return false;
-            else return true;
-        })
-
-        //if there are still phone numbers that exist at this point, place them on object
-       if (failed.length > 0) res.locals.object = {...res.locals.object, phone: failed}
-    }
-   
-    return next()
+  }catch(error){
+    return next(error);
+  }
     
 }
 
