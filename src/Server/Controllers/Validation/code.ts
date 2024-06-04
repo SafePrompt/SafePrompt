18ee7ac9c98d78002c89e6403a6315295dd6ce1a
rmarkowi1990 <rick@fastcasual.co>
2024-06-03 17:33:32 -0700

    updated syntax

--- src/Server/Controllers/Validation/code.ts
+++ src/Server/Controllers/Validation/code.ts
@@ -2,6 +2,37 @@ import middleware from '../../Types/middleware'
 
 const code: middleware = (req, res, next) => {
 
+
+
+
+    /*
+    const
+    var
+    if(
+    if (
+    for ( let
+    //
+    elif
+    ==
+    ===
+    !==
+    !=
+    &&
+    ||
+    ++
+    --
+    +=
+    <=
+    >=
+    console.log
+    print()
+    =>
+
+
+
+
+    */
+
 }
 
 export default code;
--- src/Server/Controllers/Validation/ein.ts
+++ src/Server/Controllers/Validation/ein.ts
@@ -6,15 +6,17 @@ const ein: middleware = (req, res, next) => {
 
         const prompt:string = res.locals.prompt;
 
-        const numbersRegEx = /[123456789]/
+        const unexclusiveEinFormat = /\d{2}-\d{7}/;
 
-        if (numbersRegEx.test(prompt) && prompt.includes('-')){
+        //if prompt includes at least 1 ein
+        if (unexclusiveEinFormat.test(prompt)){
 
             const failed: string[] = [];
 
             const einFormat = /^\d{2}-\d{7}$/;
             const exclude = /[123456789-]/;
 
+            //iterate through prompt finding all eins
             for (let i = 0; i < prompt.length; i++){
                 const test:string = prompt.slice(i,i+10);
                 if (einFormat.test(test) && !exclude.test(prompt[i-1]) && !exclude.test(prompt[i+10])){
@@ -22,11 +24,10 @@ const ein: middleware = (req, res, next) => {
                 }
             }
 
+            //and add to response object
             if (failed.length > 0){
                 res.locals.object = {...res.locals.object, ein: failed}
             }
-
-
 
         }
 
