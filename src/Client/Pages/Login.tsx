79021b9131dd575a96292dcd181281ceb1b5ddca
rmarkowi1990 <rick@fastcasual.co>
2024-06-05 07:49:27 -0700

    most recent rick-auth

--- src/Client/Pages/Login.tsx
+++ src/Client/Pages/Login.tsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './Login.css';
 
+const Login: React.FunctionComponent = () => {
 
+  return (
 
-
-const Login(): React.FC = ()=> {
-
-
-return (
-    <>
-    </>
-
-
-)
-
+        
+        <div className = 'container'>
+        <div className = 'inputSection'>
+        {/* <div className = 'nav'>Admin</div> */}
+            <div className = 'inputField'>
+                <h3>Username</h3>
+                <input></input>
+            </div>
+            <div className = 'inputField'>
+                <h3>Password</h3>
+                <input></input>
+            </div>
+            <div className = 'buttonSection'>
+                <button>Log In</button>
+                <button>Sign Up</button>
+            </div>
+        </div>
+    </div>
+  )
 }
+
+export default Login;
