ba9829800b25b8e01f7b3915813b702ec4d22767
cmagorr1 <71301902+cmagorr1@users.noreply.github.com>
2024-06-04 21:23:13 -0400

    react-router-dom implemented

--- src/Client/Components/MenuBar.tsx
+++ src/Client/Components/MenuBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './MenuBar.css';
 
 const MenuBar: React.FC = () => {
@@ -6,15 +7,17 @@ const MenuBar: React.FC = () => {
     <div className="menu-bar">
       <h1 className="menu-title">SafePrompt</h1>
       <div className="menu-item">
-        <label htmlFor="prompts-dropdown" className="menu-label">Prompts</label>
+        <Link to="/settings" className="menu-link">Settings</Link>
+      </div>
+      <div className="menu-item">
+        <Link to="/prompts" className="menu-link">Prompts</Link>
+      </div>
+      <div className="menu-item">
         <select id="prompts-dropdown" className="menu-dropdown">
           <option value="prompt1">Prompt 1</option>
           <option value="prompt2">Prompt 2</option>
           <option value="prompt3">Prompt 3</option>
         </select>
-      </div>
-      <div className="menu-item">
-        <span className="menu-label">Settings</span>
       </div>
     </div>
   );
