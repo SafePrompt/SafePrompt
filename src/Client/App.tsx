d9459bdcc0e83f74f64a6884c5ceb94f96331d05
cmagorr1 <71301902+cmagorr1@users.noreply.github.com>
2024-06-04 21:16:37 -0400

    moving react router dom stuff

--- src/Client/App.tsx
+++ src/Client/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Landing from './Pages/Landing';
 import Main from './Pages/Main';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Settings from './Pages/Settings';
+import Prompts from './Pages/Prompts';
 
 const App: React.FunctionComponent = () => {
     const [inputText, setInputText] = useState('');
@@ -10,9 +13,14 @@ const App: React.FunctionComponent = () => {
 
 
     return (
-        <div>
-            <Main/>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/main" element={<Main />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/prompts" element={<Prompts />} />
+            </Routes>
+        </Router>
     )
 }
 
