84d8f09a3493c1dcd4830c58602a879d6903f1ae
Matt Allen <matt.w.allen@gmail.com>
2024-06-05 18:50:25 -0700

    added check boxex for filters

--- src/Client/Pages/AdminView.tsx
+++ src/Client/Pages/AdminView.tsx
@@ -1,4 +1,4 @@
-// checkbox for each filter field
+// √ checkbox for each filter field
 // submit button
 // keyword entry option (Keyword/type)
 
@@ -8,8 +8,30 @@ const AdminView: React.FunctionComponent = () => {
     const filters: string[] = ['Currency', 'Email', 'EIN', 'SSN', 'Phone', 'Keyword(s)']
     const [selectedFilters, setSelectedFilters] = useState<string[]>([])
 
+    const handleFilterChange = (filter: string) => {
+      setSelectedFilters((prev) =>
+        prev.includes(filter)
+          ? prev.filter((item) => item !== filter)
+          : [...prev, filter]
+      );
+    };
+
   return (
-    <div>AdminView</div>
+    <><div>AdminView</div>
+    <h3>Filters:</h3><ul>
+      {filters.map((filter) => (
+        <li key={filter}>
+          <label>
+            <input
+              type="checkbox"
+              value={filter}
+              checked={selectedFilters.includes(filter)}
+              onChange={() => handleFilterChange(filter)} />
+            {' '}{filter}
+          </label>
+        </li>
+      ))}
+    </ul><p>Selected filters: {selectedFilters.join(', ') || 'None'}</p></>
   )
 }
 
